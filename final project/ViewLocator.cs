adb562d0d68675904d0ddc92219957a21b594846
MatousJetensky <matous.jetensky@gmail.com>
2026-05-26 13:16:37 +0200

    přidání souboru ViewLocator.cs

---
+++ final project/ViewLocator.cs
@@ -0,0 +1,31 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using final_project.ViewModels;
+
+namespace final_project;
+
+public class ViewLocator : IDataTemplate
+{
+    public Control? Build(object? param)
+    {
+        if (param is null)
+            return null;
+
+        var name = param.GetType().FullName!
+            .Replace("final_project.ViewModels", "final_project.Views")
+            .Replace("ViewModel", "View");
+
+        var type = Type.GetType(name);
+
+        if (type != null)
+            return (Control)Activator.CreateInstance(type)!;
+
+        return new TextBlock { Text = "Not Found: " + name };
+    }
+
+    public bool Match(object? data)
+    {
+        return data is ViewModelBase;
+    }
+}
